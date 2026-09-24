d3399a514289b767572f87d78a78cc0e5f6752ed
VivekVenigalla <46229791+VivekVenigalla@users.noreply.github.com>
2026-09-24 13:29:46 -0400

    Main application

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,34 @@
 
 int main() { //int argc, char* argv[] not used right now to circumvent warnings
 	std::cout << "Running main" << std::endl;
-	Simplex triangle({2, 0, 1});
+	
+	/*PointCloud<3> temp; 
+	// intiialize a point cloud in R^3
+	std::array<double, 3> tempPoint= {1.0, 2.0, 3.0};
+	temp.addPoint(tempPoint);*/
+
+	Simplex triangle({2, 0, 1}); // triangle simplex
+	std::cout << "Triangle Simplex " << std::endl;
 	Simplex::printSimplex(triangle);
+	std::cout << "Triangle Simplex Faces" << std::endl;
 	for (const auto& face : triangle.getFaces()) {
 		Simplex::printSimplex(face);
 	}
-	std::cout << "Same simplex from different order : " << (triangle == Simplex({0, 1, 2})) << std::endl;
+	//std::cout << "Same simplex from different order : " << (triangle == Simplex({0, 1, 2})) << std::endl;
 
-	/*PointCloud<3> temp; // intiialize a point cloud in R^3
-	std::array<double, 3> tempPoint= {1.0, 2.0, 3.0};
-	temp.addPoint(tempPoint);*/
-	Simplex tetrahedron({3, 1, 0, 2});
-	Simplex::printSimplex(tetrahedron);
+	Simplex tetrahedron({3, 1, 0, 2}); //create sample simplex
+	std::cout << "Tetrahedron Simplex" << std::endl;
+	Simplex::printSimplex(tetrahedron); // print the simplex
+	std::cout << "Tetrahedron Simplex Faces" << std::endl;
 	for (const auto& face : tetrahedron.getFaces()) {
 		Simplex::printSimplex(face);
 		for (const auto& edge : face.getFaces()) {
 			Simplex::printSimplex(edge);
 		}
 	}
+
+	// additional tests
+	std::cout << "Additional tests: " << std::endl;
 	Point a(2, {1.0, 2.0});
 	Point b(2, {3.0, 4.0});
 	Point sum = a + b;
