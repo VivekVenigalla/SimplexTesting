2ce18311bac2c4b244ab1bfd6710946f0ca72b84
VivekVenigalla <46229791+VivekVenigalla@users.noreply.github.com>
2026-09-24 13:02:38 -0400

    simplex implementation

--- include/simplex.hpp
+++ include/simplex.hpp
@@ -1,13 +1,16 @@
-
 #include <string>
 #include <iostream>
-
+#include <vector>
+#include "./include/pointCloud.hpp"
+#include "./include/point.hpp"
 
 class Simplex {
 	public:
-		Simplex(int order);
+		Simplex(int order, PointCloud& ref);
 		int returnOrder();
+		void addIndices(std::vector<int> ind);
 	private:
 		int order;
-		
+		PointCloud& pcRef; // reference to point cloud of all points
+		std::vector<int> indicesOfPoints; // indices of select points in point cloud as part of simplex
 };
--- src/simplex.cpp
+++ src/simplex.cpp
@@ -1,7 +1,15 @@
 #include "../include/simplex.hpp"
 
-Simplex::Simplex(int i_order) : order(i_order){
+Simplex::Simplex(int i_order, PointCloud& ref) : order(i_order), pcRef(ref){
 	std::cout << "Simplex object with order " << this->order << " created" << std::endl;
+}
+
+void Simplex::addIndices(std::vector<int> ind) {
+	if ((int)ind.size() !=  order + 1) {
+		std::cout << "Order is not equal to the |indices| + 1" << std::endl;
+	} else {
+		this->indicesOfPoints = ind;
+	}
 }
 
 int Simplex::returnOrder() {
